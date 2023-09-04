a249cc53154396616a314d68b58a99e081e072b7
Dilettante <ikeike@yandex.ru>
2023-09-04 22:28:35 +0300

    Update trainer

--- MultiplicationTableTrainer.py
+++ MultiplicationTableTrainer.py
@@ -11,35 +11,51 @@ import time
 #Sounds (seem to use much space in the built package)
 import winsound
 
-
 #Preps
-version = "0.100"
+VERSION = "0.100"
 
 ##Variables preps
 ##Create and init some variables for future use 
-questionsTotal = 0
+""" questionsTotal = 0
 answersTotal = 0
 answersCorrect = 0
 timeSpent = 0
 timePenalty = 0
-averageTimeSpent = 0
+averageTimeSpent = 0 """
 
 ##Init random
 random.seed()
 
 #SaiHai
-inp = input("#" + str(i) + ": How much is {a} * {b}? ".format(a=a, b=b))
 
-if inp == "q":
-    answersTotal += 1
+print("Hi!")
 
-try:
-    int(inp)
-except:
-    print("ERROR, probably you have not provided a number")
-    inp = 0
+do_stop = 0
 
+while (do_stop != 1):
+    a = random.randrange(2, 10)
+    b = random.randrange(2, 10)
+    c = a * b
+    d = random.randrange(2, 100)
+    e = random.randrange(2, 100)
 
-print("This is the multiplication Table knowledge test v." + version)
-print("Hi! Now I will ask some 14questions, and you will type the answers.")
-print("Submit numeric values, or 'q' to quit")
+    position_list = [1, 2, 3]
+    position_of_c = position_list[random.randrange(0, 3)] 
+    print("position of c is: " + str(position_of_c))   
+    position_list.remove(position_of_c)
+    print("new list is: " + str(position_list))
+
+    position_of_d = position_list[random.randrange(0, 2)]    
+    print("position of d is: " + str(position_of_d))     
+    position_list.remove(position_of_d)
+    print("new list is: " + str(position_list))
+
+    position_of_e = position_list[0]
+    print("position of e is: " + str(position_of_e))     
+    
+      
+    print(position_of_c) 
+    print(position_of_d) 
+    print(position_of_e) 
+    print("----------")
+
